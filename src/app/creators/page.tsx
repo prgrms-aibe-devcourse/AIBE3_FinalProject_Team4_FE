72e24700e20d4b367ef9f9bbd74a23895d04a1da
jooky <jooky0327@naver.com>
2025-11-28 23:00:01 +0900

    불필요 주석 제거

--- src/app/creators/page.tsx
+++ src/app/creators/page.tsx
@@ -6,7 +6,6 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export default async function CreatorsPage() {
   const creators = await fetchCreators();
-  console.log('Fetched creators:', creators);
 
   return (
     <main className="flex justify-center px-4 py-10">
@@ -19,15 +18,17 @@ export default async function CreatorsPage() {
 }
 
 async function fetchCreators() {
-  const cookieStore = cookies();
-  const cookieString = cookieStore.toString();
+  const cookieStore = await cookies();
+  const cookieHeader = cookieStore
+    .getAll()
+    .map((c) => `${c.name}=${c.value}`)
+    .join('; ');
   const res = await fetch(`${API_BASE_URL}/api/v1/users/creators`, {
     method: 'GET',
     headers: {
-      cookie: cookieString,
+      cookie: cookieHeader,
     },
   });
-  console.log('Fetch creators response status:', res.status);
   if (res.ok) {
     const data = await res.json();
     return data.data as Creator[];
