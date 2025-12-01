9de4184eefdaf08ffe082b26bbf9425a9f29caaa
leejiyeon <sky75013@naver.com>
2025-12-01 10:59:27 +0900

    feat: 스크롤 내리기

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode, useState } from 'react';
 
 export default function ReactQueryProvider({ children }: { children: ReactNode }) {
@@ -21,7 +22,7 @@ export default function ReactQueryProvider({ children }: { children: ReactNode }
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
